6afd13a94082ddee62f2d1618a938734e98a3ffd
Mohammed Zain ul Hassan <powehi29@gmail.com>
2024-09-07 13:22:45 +0500

    configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,31 @@
 import PatientForm from "@/components/forms/PatientForm";
 import PassKeyModal from "@/components/PassKeyModal";
-import Image from "next/image"
 import Link from "next/link";
+import { GridBackground } from "@/components/ui/GridBackground";
 
-export default function Home({searchParams}:SearchParamProps) {
+export default function Home({ searchParams }: SearchParamProps) {
   const isAdmin = searchParams.admin === 'true';
-  
+
   return (
-    <div className="flex h-screen max-h-screen">
-      { isAdmin && <PassKeyModal/> }
-      
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
-          <Image 
-            src= "/assets/icons/logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
-          />
+    <GridBackground>
+      <div className="flex items-center justify-center h-screen">
+        {isAdmin && <PassKeyModal />}
+        
+        <div className="max-w-[496px] w-full text-center">
+          <h1 className="mb-12 text-2xl font-bold">Welcome to CarePulse</h1>
 
-          <PatientForm/>
+          <PatientForm />
 
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
+          <div className="text-sm mt-8 flex justify-between">
+            <p className="text-dark-600">
               © 2024 CarePulse
             </p>
-            <Link href = "/?admin=true" className="text-green-500">Admin</Link>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
           </div>
         </div>
-      </section>
-      <Image
-  	    src="/assets/images/onboarding-img.png"
-        height={1000}
-        width={1000}
-        alt = "patient"
-        className="side-img max-w-[50%]"
-      />
-    </div>
+      </div>
+    </GridBackground>
   );
 }
