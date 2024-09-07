ed7754085da0a8b9efabca3722db4c838e28b061
Mohammed Zain ul Hassan <powehi29@gmail.com>
2024-09-07 13:12:23 +0500

    setup

--- components/ui/blockQuote.tsx
+++
@@ -1,7 +0,0 @@
-export function TypographyBlockquote({text, className}:{text : string, className?: string}) {
-  return (
-    <blockquote className={`mt-6 border-l-2 pl-6 italic ${className}`}>
-      {text}
-    </blockquote>
-  )
-}
